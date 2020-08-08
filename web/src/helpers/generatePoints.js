8dbb396ac0a008d77cd1fb2dce9f917fa69446fe
gorgonfiredotcom <gorgonfire@gmail.com>
2020-08-07 20:53:50 -0400

    Basic Poisson Disk Generation Function

--- web/src/helpers/generatePoints.js
+++ web/src/helpers/generatePoints.js
@@ -3,7 +3,14 @@
 // Generate random points using poisson disc sampling
 
 function generatePoints (radius, regionX, regionY, packing) {
-  
+    var sampling = new PoissonDiskSampling({
+        shape: [regionX, regionY],
+        minDistance: radius,
+        maxDistance: 2 * radius,
+        tries: packing
+    });
+    
+    return sampling.fill();
 }
 
 export default generatePoints
